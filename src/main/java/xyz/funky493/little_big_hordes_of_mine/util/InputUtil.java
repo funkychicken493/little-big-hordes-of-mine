6cc429ee67c29a7ca2b85d58da299cbdd85482a0
funkychicken493 <82616867+funkychicken493@users.noreply.github.com>
2023-11-13 10:08:22 -0600

    wip

---
+++ src/main/java/xyz/funky493/little_big_hordes_of_mine/util/InputUtil.java
@@ -0,0 +1,18 @@
+package xyz.funky493.little_big_hordes_of_mine.util;
+
+import java.util.regex.Matcher;
+
+public class InputUtil {
+    public static boolean checkComparatorString(String comparatorString, Float value) {
+        // Example inputs:
+        // x>=1
+        // x<=1
+        // x==1
+        // x!=1
+        // x>1
+        // x<1
+        // 2>x>1
+
+        return false;
+        }
+}
